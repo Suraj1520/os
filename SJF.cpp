f86ccb778a44739c3580c8db20e058c492f247d7
Suraj Kumar <91044355+Suraj1520@users.noreply.github.com>
2022-11-13 13:44:45 +0530

    Create SJF.cpp

---
+++ SJF.cpp
@@ -0,0 +1,204 @@
+SJF -NON Premptitive
+
+#include <stdio.h>
+int main()
+{
+	int A[100][4]; // Matrix for storing Process Id, Burst
+				// Time, Average Waiting Time & Average
+				// Turn Around Time.
+	int i, j, n, total = 0, index, temp;
+	float avg_wt, avg_tat;
+	printf("Enter number of process: ");
+	scanf("%d", &n);
+	printf("Enter Burst Time:\n");
+	// User Input Burst Time and alloting Process Id.
+	for (i = 0; i < n; i++) {
+		printf("P%d: ", i + 1);
+		scanf("%d", &A[i][1]);
+		A[i][0] = i + 1;
+	}
+	// Sorting process according to their Burst Time.
+	for (i = 0; i < n; i++) {
+		index = i;
+		for (j = i + 1; j < n; j++)
+			if (A[j][1] < A[index][1])
+				index = j;
+		temp = A[i][1];
+		A[i][1] = A[index][1];
+		A[index][1] = temp;
+
+		temp = A[i][0];
+		A[i][0] = A[index][0];
+		A[index][0] = temp;
+	}
+	A[0][2] = 0;
+	// Calculation of Waiting Times
+	for (i = 1; i < n; i++) {
+		A[i][2] = 0;
+		for (j = 0; j < i; j++)
+			A[i][2] += A[j][1];
+		total += A[i][2];
+	}
+	avg_wt = (float)total / n;
+	total = 0;
+	printf("P	 BT	 WT	 TAT\n");
+	// Calculation of Turn Around Time and printing the
+	// data.
+	for (i = 0; i < n; i++) {
+		A[i][3] = A[i][1] + A[i][2];
+		total += A[i][3];
+		printf("P%d	 %d	 %d	 %d\n", A[i][0],
+			A[i][1], A[i][2], A[i][3]);
+	}
+	avg_tat = (float)total / n;
+	printf("Average Waiting Time= %f", avg_wt);
+	printf("\nAverage Turnaround Time= %f", avg_tat);
+}
+
+
+
+
+
+
+
+Premptive
+
+// C++ program to implement Shortest Remaining Time First
+// Shortest Remaining Time First (SRTF)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Process {
+	int pid; // Process ID
+	int bt; // Burst Time
+	int art; // Arrival Time
+};
+
+// Function to find the waiting time for all
+// processes
+void findWaitingTime(Process proc[], int n,
+								int wt[])
+{
+	int rt[n];
+
+	// Copy the burst time into rt[]
+	for (int i = 0; i < n; i++)
+		rt[i] = proc[i].bt;
+
+	int complete = 0, t = 0, minm = INT_MAX;
+	int shortest = 0, finish_time;
+	bool check = false;
+
+	// Process until all processes gets
+	// completed
+	while (complete != n) {
+
+		// Find process with minimum
+		// remaining time among the
+		// processes that arrives till the
+		// current time`
+		for (int j = 0; j < n; j++) {
+			if ((proc[j].art <= t) &&
+			(rt[j] < minm) && rt[j] > 0) {
+				minm = rt[j];
+				shortest = j;
+				check = true;
+			}
+		}
+
+		if (check == false) {
+			t++;
+			continue;
+		}
+
+		// Reduce remaining time by one
+		rt[shortest]--;
+
+		// Update minimum
+		minm = rt[shortest];
+		if (minm == 0)
+			minm = INT_MAX;
+
+		// If a process gets completely
+		// executed
+		if (rt[shortest] == 0) {
+
+			// Increment complete
+			complete++;
+			check = false;
+
+			// Find finish time of current
+			// process
+			finish_time = t + 1;
+
+			// Calculate waiting time
+			wt[shortest] = finish_time -
+						proc[shortest].bt -
+						proc[shortest].art;
+
+			if (wt[shortest] < 0)
+				wt[shortest] = 0;
+		}
+		// Increment time
+		t++;
+	}
+}
+
+// Function to calculate turn around time
+void findTurnAroundTime(Process proc[], int n,
+						int wt[], int tat[])
+{
+	// calculating turnaround time by adding
+	// bt[i] + wt[i]
+	for (int i = 0; i < n; i++)
+		tat[i] = proc[i].bt + wt[i];
+}
+
+// Function to calculate average time
+void findavgTime(Process proc[], int n)
+{
+	int wt[n], tat[n], total_wt = 0,
+					total_tat = 0;
+
+	// Function to find waiting time of all
+	// processes
+	findWaitingTime(proc, n, wt);
+
+	// Function to find turn around time for
+	// all processes
+	findTurnAroundTime(proc, n, wt, tat);
+
+	// Display processes along with all
+	// details
+	cout << " P\t\t"
+		<< "BT\t\t"
+		<< "WT\t\t"
+		<< "TAT\t\t\n";
+
+	// Calculate total waiting time and
+	// total turnaround time
+	for (int i = 0; i < n; i++) {
+		total_wt = total_wt + wt[i];
+		total_tat = total_tat + tat[i];
+		cout << " " << proc[i].pid << "\t\t"
+			<< proc[i].bt << "\t\t " << wt[i]
+			<< "\t\t " << tat[i] << endl;
+	}
+
+	cout << "\nAverage waiting time = "
+		<< (float)total_wt / (float)n;
+	cout << "\nAverage turn around time = "
+		<< (float)total_tat / (float)n;
+}
+
+// Driver code
+int main()
+{
+	Process proc[] = { { 1, 6, 2 }, { 2, 2, 5 },
+					{ 3, 8, 1 }, { 4, 3, 0}, {5, 4, 4} };
+	int n = sizeof(proc) / sizeof(proc[0]);
+
+	findavgTime(proc, n);
+	return 0;
+}
